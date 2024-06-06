ff0cea95d137520536244dad7b3c22e3bce2d381
parnianshamsa <153368555+parnianshamsa@users.noreply.github.com>
2024-06-07 02:04:46 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    new FaalGUI();
+    }
+}
